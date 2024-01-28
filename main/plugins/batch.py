046d42bfafac6eed6b37bf468c8387b5bdc564fa
Gittu2raj11 <152615267+Gittu2raj11@users.noreply.github.com>
2024-01-28 16:48:53 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -1,5 +1,5 @@
-#Tg:MaheshChauhan/DroneBots
-#Github.com/Vasusen-code
+#Tg: Rajbhadani/@RajPrimebots
+#Github.com/Gittu2raj11/Raazsave2bot
 
 """
 Plugin for both public & private channels!
@@ -42,7 +42,7 @@ async def _batch(event):
         await event.reply(r)
         return       
     if event.sender_id in batch:
-        return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
+        return await event.reply("You've already started one batch, sir please wait for it to complete, Muft me itna hi milega!")
     async with Drone.conversation(event.chat_id) as conv: 
         if s != True:
             await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
